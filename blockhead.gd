216ef8ae97850e2d9c4ca1e87f8bc11bca4eb034
Dhruval prajapati <thedhruvalprajapati@gmail.com>
2026-07-14 23:49:55 +0530

    WASD movement, mouse movements using camera (rudimentary, will add restraints or spring arm later), mouse mode captured-visible upon game beginning-pressing ui_cancel(esc)

--- blockhead.gd
+++ blockhead.gd
@@ -4,8 +4,25 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 12
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	#new camera movement
+	if event is InputEventMouseMotion:
+		$Camera_Controller.rotation_degrees.y -= event.relative.x * 0.5
+		$Camera_Controller.rotation_degrees.x -= event.relative.y * 0.5
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta):
+	
+	# old camera movement
+	## Camera movement left / Right
+	#if Input.is_action_just_pressed("cam_left"):
+		#$Camera_Controller.rotate_y(deg_to_rad(-30))
+	#if Input.is_action_just_pressed("cam_right"):
+		#$Camera_Controller.rotate_y(deg_to_rad(30))
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,9 +33,13 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
+	
+	# New vector3 direction, taking into account the user movement inputs and the camera rotation
+	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Rotate player mesh so its oriented towards movement according to the camera
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
